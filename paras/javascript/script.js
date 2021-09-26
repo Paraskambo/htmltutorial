e1fd9308aa622a0fed8c5a40eb0565a24107173a
Vipin Kumar <vipinkmboj20@gmail.com>
2021-09-26 13:44:46 +0530

    javaScript

--- paras/javascript/script.js
+++ paras/javascript/script.js
@@ -29,10 +29,10 @@ var cup = 'tea';
     var b = 14;
     var total = a + b;
 
-    var name = 'Paras Kamboj';
+   // var name = 'Paras Kamboj';
 
 
-    console.log('kamboj kumar');
+    //console.log('kamboj kumar');
     /*
     var car = {
         modal: 'Toyota',
@@ -49,3 +49,98 @@ var cup = 'tea';
     */
     document.getElementById('total').innerHTML = name;
     
+    //alert('Kamboj');
+
+
+    var x = 'Paras';
+    var y = ' Kamboj';
+    console.log(x + y);
+
+    var age = 20;
+
+    age = 25;
+
+    console.log(age)
+
+    var fruit;
+
+    fruit = 'Apple';
+    if(fruit == undefined) {
+        console.log('Fruit is not defined!');
+    } else {
+        console.log('Fruit is defined and fruit name is ' + fruit);
+    }
+
+    var electricity;
+    electricity = 'on';
+    if(electricity == undefined) {
+        console.log('Fan is not running!');
+    } else {
+        console.log('Fan is running!');
+    }
+    
+    var power = 'full';
+
+    if(power == 'dim') {
+        console.log('Bulb light is dim');
+    } else {
+        console.log('Bulb is not dim');
+    }
+
+    var age = 19;
+/*
+    if(age < 18) {
+        console.log('You can not drive');
+    } else if(age == 18) {
+        console.log('Show me your driving licence and drive');
+    } else {
+        console.log('You can drive');
+    }
+*/
+    if(age == 18 || age > 18 ) {
+        console.log('Yes, you can drive');
+    } else {
+        console.log('Sorry, you are not eligible to drive');
+    }
+
+    const namee = 'Paras Kumar Kamboj'
+    const myAge = 30;
+    const rating = 4.5;
+    const isCool = true;
+    const d = null;
+    const e = undefined;
+    let f;
+
+    console.log(namee);
+    console.log(typeof namee);
+    console.log(myAge);
+    console.log(typeof myAge);
+
+    console.log(rating);
+    console.log(typeof rating);
+
+    console.log(isCool);
+    console.log(typeof isCool);
+
+    console.log(d);
+    console.log(typeof d);
+
+    console.log(e);
+    console.log(typeof e);
+
+    console.log(f);
+    console.log(typeof f);
+    
+    document.getElementById('ghj').innerHTML = 'My name is ' + namee + ' and My age is ' + myAge + ' and I am not a ' + typeof isCool;
+
+    document.getElementById('ghjk').innerHTML = `My name is ${namee} and my age is ${myAge} and I am not a ${typeof isCool}`  //'My name is ' + namee + ' and My age is ' + myAge + ' and I am not a ' + typeof isCool;
+
+/*
+    if(typeof namee === "string") {
+        console.log(namee + ' is a string type');
+    }
+
+    if(typeof myAge != 'string') {
+        console.log('My age is a ' + myAge + 'which is a ' + typeof myAge);
+    }
+    */
